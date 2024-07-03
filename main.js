8fdb105aa296ac414d46856898f039044a54f8c7
Alexander Leali <aleali@bgsu.edu>
2024-07-03 11:49:59 -0400

    changed some debugging code to be more professional

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 //idk know at this point
-console.log("normal log: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+console.log("normal log: Script is running");
 
-document.createElement("p");
+createElem();
+
 let recipeDiv = document.getElementsByTagName('div');
-recipeDiv.innerHTML = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+recipeDiv.innerHTML = "This is the Div";
 
 window.addEventListener("DOMContentLoaded", domLoaded);
 
 function domLoaded(){
-    console.log("window load: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    console.log("window load: Scrit runs again");
     //let theWholeWebsite = document.body;
     //let recipeDiv = $("#recipe")
     let recipeDiv = document.getElementsByTagName('div');
-    recipeDiv.innerHTML = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+    recipeDiv.innerHTML = "This is the recipie div";
 }
 
+function createElem(){
+    var newElem = document.createElement("p");
+    newElem.innerHTML = "This is a newly made paragaph element";
+
+    return newElem.firstChild;
+}
