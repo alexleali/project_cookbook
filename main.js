3dd86ff10aa63175ade065d6f7fe40258cd1e356
Alexander Leali <aleali@bgsu.edu>
2024-07-03 11:52:51 -0400

    added some documentation in code form

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-//idk know at this point
+//Check to see if the script in running
 console.log("normal log: Script is running");
 
+//debug function
 createElem();
 
+//more debugging code
 let recipeDiv = document.getElementsByTagName('div');
 recipeDiv.innerHTML = "This is the Div";
 
+//run function when webpage is loaded
 window.addEventListener("DOMContentLoaded", domLoaded);
 
+//script's main function
 function domLoaded(){
+    //know that the script runs again
     console.log("window load: Scrit runs again");
     //let theWholeWebsite = document.body;
     //let recipeDiv = $("#recipe")
@@ -16,6 +21,7 @@ function domLoaded(){
     recipeDiv.innerHTML = "This is the recipie div";
 }
 
+//debug function
 function createElem(){
     var newElem = document.createElement("p");
     newElem.innerHTML = "This is a newly made paragaph element";
